ca59b76a39ed3dc3703d2b6aa258194ff4bfba7f
xiayu0321 <xiayugogo0321@163.com>
2016-08-08 19:47:39 +0800

    gold data

--- golden-data-react-redux/app.js
+++ golden-data-react-redux/app.js
@@ -1,7 +1,28 @@
 const app = React.createClass({
+    getInitialState:function () {
+      return {
+          elements:[]
+      }
+    },
+    removeElement:function (index) {
+        console.log('remove',index);
+        const elements = this.state.elements;
+        elements.splice(index,1);
+        this.setState({elements});
+    },
+    addElement:function (element) {
+        console.log('add element',element);
+        const elements = this.state.elements;
+        elements.push(element);
+        this.setState({elements});
+    },
     render:function () {
         return <div>
-            {this.props.children}
+            {this.props.children && React.cloneElement(this.props.children,{
+                elements:this.state.elements,
+                onRemove:this.removeElement,
+                onAdd:this.addElement
+            })}
         </div>
     }
 });
@@ -10,16 +31,55 @@ const Editor = React.createClass({
     render:function () {
         return <div>
             <ReactRouter.Link to="/preview">preview</ReactRouter.Link>
-            Editor
+            <LeftPanel elements={this.props.elements} onRemove={this.props.onRemove} />
+            <RightPanel onAdd={this.props.onAdd}/>
         </div>
+    }
+});
+
+const LeftPanel = React.createClass({
+    remove:function (index) {
+      this.props.onRemove(index)
+    },
+   render:function () {
+       const elements =this.props.elements.map((ele,index) => {
+           return <div key={index}>
+               <input type={ele} />
+               <button onClick={this.remove.bind(this,index)}>X</button>
+           </div>
+       });
+       return <div>
+           {elements}
+       </div>
+   }
+});
+
+const RightPanel = React.createClass({
+    add:function () {
+        const element =$("input[name=element]:checked").val()
+        this.props.onAdd(element);
+    },
+    render:function () {
+        return <div>
+            <div>
+                <input type="radio" name="element" value="text"/>Text
+                <input type="radio" name="element" value="date"/>Date
+            </div>
+            <button onClick={this.add}>+</button>
+            </div>
     }
 });
 
 const preview = React.createClass({
     render:function () {
+        const elements =this.props.elements.map((ele,index) => {
+           return <div key={index}>
+               <input type={ele}/>
+           </div>;
+        });
         return <div>
             <ReactRouter.Link to="/">Editor</ReactRouter.Link>
-            preview
+            {elements}
         </div>
     }
 });
